b3b987eebe49da362fd15e95b39b4167cf867682
otabekkadirov <ganijanovich94@gmail.com>
2022-04-19 07:39:31 +0500

    "feat: Add playRound() and game() functions

--- index.js
+++ index.js
@@ -26,4 +26,64 @@ function humanPlay(userInput) {
   }
 }
 
-let computerSelection = computerPlay();
+// Let's make a function that plays a single round with userInput and computerInput.
+function playRound(playerSelection, computerSelection) {
+  // First we need to check when the round is tie
+  if (playerSelection === computerSelection) {
+    return "tie";
+
+    // Then we check every case when the user wins
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "user won";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "user won";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "user won";
+
+    // This one is a secret cheat code
+  } else if (playerSelection === "bomb") {
+    return "user won";
+  } else {
+    return "computer won";
+  }
+}
+
+// Let's make a function that plays a game of 5 rounds.
+function game() {
+  // We need to keep the scores of human and computer.
+  let humanScore = 0;
+  let computerScore = 0;
+
+  // We call playRound() function 5 times.
+  for (let i = 0; i <= 5; i++) {
+    // playRound() function gets 2 input, so we prompt the user for an input.
+    let userWeapon = prompt("Choose your weapon: Rock, Paper or Scissors");
+    let playerSelection = humanPlay(userWeapon);
+    let computerSelection = computerPlay();
+    // We store the result of each round to increment the scores of the players.
+    let result = playRound(playerSelection, computerSelection);
+    console.log("i: " + i);
+
+    // When players win the round, we increment their score
+    if (result === "user won") {
+      humanScore++;
+      console.log("human: " + humanScore);
+    } else if (result === "computer won") {
+      computerScore++;
+      console.log("computer: " + computerScore);
+    }
+    // If the round is tie, replay the round;
+    if (result === "tie") {
+      i--;
+      console.log("round: tie");
+    }
+    // Whoever scores 3 wins.
+    if (humanScore === 3) {
+      return `Game over. Human ${humanScore}: Computer ${computerScore} \nHuman won`;
+    } else if (computerScore === 3) {
+      return `Game over. Human ${humanScore}: Computer ${computerScore} \nComputer won`;
+    }
+  }
+}
+
+console.log(game());
